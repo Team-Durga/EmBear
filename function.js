606861b8f71220f3edff252ea453db1a334cf3ec
Team-Durga <marko09kaptas@gmail.com>
2025-03-29 23:08:08 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,79 @@
+function Tagok(){
+    window.location.href = "tagok.html"
+}
+
+function Autizmus(){
+    window.location.href = "autizmus.html"
+}
+
+function Innovacio(){
+    window.location.href = "innovacio.html"
+}
+
+function Durga(){
+    window.location.href = "durga.html"
+}
+
+function Jatek(){
+    window.location.href = "jatek.html"
+}
+
+function Verseny(){
+    window.location.href = "verseny.html"
+}
+
+function Fooldal(){
+    window.location.href = "index.html"
+}
+
+function Tovabb(){
+    window.location.href = "jatek1.html"
+}
+
+function Jatek1_1(){
+    window.location.href = "jatek2.html"
+}
+
+function Jatek2_4(){
+    window.location.href = "jatek3.html"
+}
+
+function Jatek3_2(){
+    window.location.href = "jatek4.html"
+}
+
+function Jatek4_2(){
+    window.location.href = "jatek5.html"
+}
+
+function Jatek5_4(){
+    window.location.href = "jatek6.html"
+}
+
+function Jatek6_1(){
+    window.location.href = "jatek7.html"
+}
+
+function Jatek7_1(){
+    window.location.href = "jatek8.html"
+}
+
+function Jatek8_4(){
+    window.location.href = "jatek9.html"
+}
+
+function Jatek9_3(){
+    window.location.href = "jatek10.html"
+}
+
+function Jatek10_2(){
+    window.location.href = "index.html"
+}
+
+function Kerdoiv(){
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSdWdsZrTbpXa52IMQxNFjq37W6qRhau5qeJqn5svjdNtx5PhQ/viewform?usp=dialog"
+}
+
+function Instagram(){
+    window.location.href = "https://www.instagram.com/durga.team/"
+}
